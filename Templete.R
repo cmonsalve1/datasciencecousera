74fd0a96682ba0a7be2962d7e09016092582983e
cmonsalve1 <cmonsalve@sbcglobal.net>
2016-06-19 13:29:33 -0700

    Add files via upload

--- Templete.R
+++ Templete.R
@@ -345,8 +345,8 @@ qnorm(quantile, mean = ??, sd = ??)
 
 #Question 4
 #Refer to the previous question. Brain volume for adult women is about 1,100 cc for women with a standard deviation of 75 cc. Consider the sample mean of 100 random adult women from this population. What is the 95th percentile of the distribution of that sample mean?
-SE <- ??/sqrt(n)
-qnorm(quantile, mean = ??, sd = SE)
+SE <- sd/sqrt(n)
+qnorm(quantile, mean = mean, sd = SE)
 
 #Question 5
 #You flip a fair coin 5 times, about what's the probability of getting 4 or 5 heads?
@@ -357,28 +357,28 @@ pbinom(quantile, size=n, prob=p, lower.tail = FALSE)
 
 #Question 6
 #The respiratory disturbance index (RDI), a measure of sleep disturbance, for a specific population has a mean of 15 (sleep events per hour) and a standard deviation of 10. They are not normally distributed. Give your best estimate of the probability that a sample mean RDI of 100 people is between 14 and 16 events per hour?
-?? <- 15
-?? <- 10
+mean <- 15
+sd <- 10
 n <- 100
-SE <- ??/sqrt(n)
+SE <- sd/sqrt(n)
 left <- 14
 right <- 16
-percentageLeft <- pnorm(left, mean = ??, sd = SE) * 100
+percentageLeft <- pnorm(left, mean = mean, sd = SE) * 100
 percentageRight <- pnorm(right, mean = ??, sd = SE) * 100
 percentageRight - percentageLeft
 
 #Question 7
 #Consider a standard uniform density. The mean for this density is .5 and the variance is 1 / 12. You sample 1,000 observations from this distribution and take the sample mean, what value would you expect it to be near?
 quantile <- 0.5
-?? <- 0.5
-?? <- 1/12
+mean <- 0.5
+sd <- 1/12
 n <- 1000
-SE <- ??/sqrt(n)
-qnorm(quantile, mean = ??, sd = SE)
+SE <- mean/sqrt(n)
+qnorm(quantile, mean = mean, sd = SE)
 
 #Question 8
 #The number of people showing up at a bus stop is assumed to be Poisson with a mean of 5 people per hour. You watch the bus stop for 3 hours. About what's the probability of viewing 10 or fewer people?
 t <- 3
-?? <- 5
+lambda <- 5
 quantile <- 10
-ppois(quantile, lambda = t * ??)
+ppois(quantile, lambda = t * lambda)
